f443850a512efd9666b479a9381c05a8e79f5cbd
husniramdani <husniramdani18@gmail.com>
2024-12-05 02:08:39 +0700

    feat: qr code

--- app/admin/ternak/columns.tsx
+++ app/admin/ternak/columns.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ColumnDef, type Row } from '@tanstack/react-table'
-import { MoreHorizontal } from 'lucide-react'
+import { MoreHorizontal, QrCode as QrCodeIcon } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
+import QRCode from 'qrcode'
 
 import {
   AlertDialog,
@@ -19,6 +21,14 @@ import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -30,56 +40,6 @@ import { dateFormat, genderFormat, statusFormat } from '@/constants/format'
 import { currencyIDR } from '@/constants/format'
 import { useDeleteTernak } from '@/hooks/services/ternak'
 import { Ternak } from '@/types/ternak'
-
-const ActionsCell = ({ row }: { row: Row<Ternak> }) => {
-  const deleteTernak = useDeleteTernak()
-  const ternakId = row.original.id
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false)
-
-  const handleDelete = () => {
-    deleteTernak.mutate(ternakId)
-    setShowDeleteAlert(false)
-  }
-
-  return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open menu {row.id}</span>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Aksi</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <Link href={`/admin/ternak/${ternakId}`}>Ubah</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setShowDeleteAlert(true)}>
-            Hapus
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-
-      <AlertDialog open={showDeleteAlert} onOpenChange={setShowDeleteAlert}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Apakah anda yakin?</AlertDialogTitle>
-            <AlertDialogDescription>
-              Tindakan ini tidak dapat dibatalkan. Data ternak ini akan dihapus
-              secara permanen.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Batal</AlertDialogCancel>
-            <AlertDialogAction onClick={handleDelete}>Hapus</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </>
-  )
-}
 
 const convertVariant = (status: string) => {
   if (status === 'AVAILABLE') return 'info'
@@ -160,7 +120,114 @@ export const columns: ColumnDef<Ternak>[] = [
     cell: ({ row }) => <div>{dateFormat(row.getValue('created_at'))}</div>,
   },
   {
+    id: 'qrcode',
+    header: () => <div>QR Code</div>,
+    cell: ({ row }) => <QRCodeModal row={row} />,
+  },
+  {
     id: 'actions',
     cell: ({ row }) => <ActionsCell row={row} />,
   },
 ]
+
+const QRCodeModal = ({ row }: { row: Row<Ternak> }) => {
+  const ternakId = row.original.id
+  const [QRCodeURL, setQRCodeURL] = useState('')
+  const baseUrl = `${window.location.protocol}//${window.location.host}`
+
+  useEffect(() => {
+    QRCode.toDataURL(
+      `${baseUrl}/admin/ternak/${ternakId}`,
+      {
+        width: 600,
+        margin: 2,
+        color: {
+          dark: '#000',
+          light: '#FFF',
+        },
+      },
+      (err, url) => {
+        if (err) return console.error(err)
+        setQRCodeURL(url)
+      },
+    )
+  }, [ternakId, baseUrl])
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size="icon">
+          <QrCodeIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-xs">
+        <DialogHeader>
+          <DialogTitle>QR Code</DialogTitle>
+          <DialogDescription>Generated QR</DialogDescription>
+        </DialogHeader>
+        <div className="border mx-auto">
+          <Image src={QRCodeURL} alt="qr-code" width={400} height={400} />
+        </div>
+        <div className="flex justify-between gap-x-4">
+          <Button variant="outline" type="button" className="w-full">
+            Share
+          </Button>
+          <Button asChild type="button" className="w-full">
+            <a href={QRCodeURL} download={`qrcode-${ternakId}.png`}>
+              Download
+            </a>
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+const ActionsCell = ({ row }: { row: Row<Ternak> }) => {
+  const deleteTernak = useDeleteTernak()
+  const ternakId = row.original.id
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false)
+
+  const handleDelete = () => {
+    deleteTernak.mutate(ternakId)
+    setShowDeleteAlert(false)
+  }
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Aksi</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Link href={`/admin/ternak/${ternakId}`}>Ubah</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setShowDeleteAlert(true)}>
+            Hapus
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AlertDialog open={showDeleteAlert} onOpenChange={setShowDeleteAlert}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Apakah anda yakin?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Tindakan ini tidak dapat dibatalkan. Data ternak ini akan dihapus
+              secara permanen.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Batal</AlertDialogCancel>
+            <AlertDialogAction onClick={handleDelete}>Hapus</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  )
+}
