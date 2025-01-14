0898977d81b629cf6bd635f0ab48407c8415f65c
husniramdani <husniramdani18@gmail.com>
2025-01-15 01:59:09 +0700

    feat: oke

--- hooks/use-pagination.tsx
+++ hooks/use-pagination.tsx
@@ -1,30 +1,30 @@
-import { useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 const usePagination = () => {
-  const searchParams = useSearchParams();
-  const initialPage = Number(searchParams.get('page')) || 1;
-  const initialLimit = Number(searchParams.get('limit')) || 10;
+  const searchParams = useSearchParams()
+  const initialPage = Number(searchParams.get('page')) || 1
+  const initialLimit = Number(searchParams.get('limit')) || 10
 
-  const [page, setPage] = useState(initialPage);
-  const [limit, setLimit] = useState(initialLimit);
+  const [page, setPage] = useState(initialPage)
+  const [limit, setLimit] = useState(initialLimit)
 
   const nextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
+    setPage((prevPage) => prevPage + 1)
+  }
 
   const prevPage = () => {
-    setPage((prevPage) => Math.max(prevPage - 1, 1)); // Prevent going below page 1
-  };
+    setPage((prevPage) => Math.max(prevPage - 1, 1)) // Prevent going below page 1
+  }
 
   const setPageNumber = (newPage: number) => {
-    setPage(newPage);
-  };
+    setPage(newPage)
+  }
 
   const setItemsPerPage = (newLimit: number) => {
-    setLimit(newLimit);
-    setPage(1); // Reset to first page when limit changes
-  };
+    setLimit(newLimit)
+    setPage(1) // Reset to first page when limit changes
+  }
 
   return {
     page,
@@ -33,7 +33,8 @@ const usePagination = () => {
     prevPage,
     setPageNumber,
     setItemsPerPage,
-  };
-};
+  }
+}
 
-export default usePagination;
+export default usePagination
+
