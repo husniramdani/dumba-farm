23ca12f07ce42354969507794b9b4d8845fe182c
husniramdani <husniramdani18@gmail.com>
2025-01-15 02:06:29 +0700

    feat: fix lagi

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
 
 import { buttonVariants } from '@/components/ui/button'
@@ -61,10 +61,10 @@ function Calendar({
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
-          <ChevronLeftIcon className={cn('h-4 w-4', className)} {...props} />
+          <ChevronLeft className={cn('h-4 w-4', className)} {...props} />
         ),
         IconRight: ({ className, ...props }) => (
-          <ChevronRightIcon className={cn('h-4 w-4', className)} {...props} />
+          <ChevronRight className={cn('h-4 w-4', className)} {...props} />
         ),
       }}
       {...props}
